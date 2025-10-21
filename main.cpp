7c6db0572a4c06efba70819de4cfbed0adea2c28
KarsuncevN <st141901>
2025-10-21 12:52:13 +0300

    feat:command line

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
-int main(){
+int main(int argc, char* argv[]){
+        unsigned int sum=0;
+        unsigned int prod=1u;
+	if (argc > 1){
+		char* number = argv[1];
+		int index = 0;
+		
+		while ( number[index] != '\0' && index < 11){
+			if(number[index] >= '0' && number[index]<='9'){
+				sum += number[index] - '0';
+				prod *= number[index] - '0';
+			}else{
+				std::cout << "wrong number" << argv[1] << "'" <<std::endl;}
+			return 1;
+			++index;
+		}
+	
+	}
 	unsigned int number;
 	std::cout<<"input your number: \n";
 	std::cin>>number;
-	
-	unsigned int sum=0;
-	unsigned int prod=1u;
 
 	do{
 		sum += number%10;
